8bda9fda9eff1fb03473bf5830871ef6a206e730
Pader <ypnow@163.com>
2023-09-21 01:47:56 +0800

    Focus only on the implementation of commands, connection and queue implementation by SimpleTextClient.

--- src/Client.php
+++ src/Client.php
@@ -4,30 +4,19 @@ namespace Wind\Redis;
 
 use Amp\Socket\ConnectContext;
 use Amp\Socket\Socket;
-use Revolt\EventLoop;
-use SplQueue;
+use Wind\Socket\SimpleTextClient;
 
 use function Amp\Socket\connect;
 
 /**
  * Wind Framework Redis Client
  */
-class Client
+class Client extends SimpleTextClient
 {
 
     use Commands;
 
     private array $config = [];
-
-    private Socket $socket;
-
-    private int $status = 0;
-
-    private const STATUS_CLOSED = 0;
-    private const STATUS_CONNECTING = 1;
-    private const STATUS_CONNECTED = 2;
-
-    private SplQueue $queue;
 
     /**
      * @var int
@@ -44,9 +33,6 @@ class Client
      */
     private $subscribes = [];
 
-    private bool $processing = false;
-    private bool $queuePaused = false;
-
     /**
      * Create redis connection instance
      *
@@ -54,6 +40,8 @@ class Client
      */
     public function __construct($connection='default')
     {
+        parent::__construct();
+
         if (is_array($connection)) {
             $this->config = $connection;
         } else {
@@ -63,62 +51,33 @@ class Client
             }
         }
 
-        $this->queue = new SplQueue;
-        $this->queue->setIteratorMode(SplQueue::IT_MODE_DELETE);
+        $this->autoReconnect = $this->config['auto_reconnect'] ?? true;
+
+        if (isset($this->config['reconnect_delay'])) {
+            $this->reconnectDelay = $this->config['reconnect_delay'];
+        }
 
         $this->connect();
     }
 
-    /**
-     * connect
-     *
-     * @return void
-     */
-    public function connect()
+    protected function createSocket(): Socket
     {
-        if ($this->status == self::STATUS_CONNECTED) {
-            return;
-        }
-
-        $this->status = self::STATUS_CONNECTING;
-
         $address = $this->config['host'].':'.($this->config['port'] ?? 6379);
         $timeout = $this->config['connect_timeout'] ?? 5;
 
         $connectContext = (new ConnectContext)
             ->withConnectTimeout($timeout);
 
-        try {
-            $this->socket = connect($address, $connectContext);
-        } catch (\Throwable $e) {
+        return connect($address, $connectContext);
+    }
 
-            $reconnect = $this->config['auto_reconnect'] ?? true;
-
-            if ($reconnect) {
-                $reconnectDelay = $this->config['reconnect_delay'] ?? 2;
-                echo "Redis reconnect after {$reconnectDelay} seconds cause: {$e->getMessage()}.\n";
-                EventLoop::delay($reconnectDelay, $this->connect(...));
-                return;
-            } else {
-                throw $e;
-            }
-        }
-
-        // $this->socket->onClose(function() {
-        //     echo "Redis connection closed\n";
-        // });
-
+    protected function authenticate()
+    {
         if ($this->password || !empty($this->config['auth'])) {
             $this->auth($this->password ?: $this->config['auth']);
         }
 
-        $this->status = self::STATUS_CONNECTED;
-
-        //重新恢复队列
-        if ($this->queuePaused) {
-            $this->queuePaused = false;
-            $this->process();
-        }
+        $this->select($this->db);
     }
 
     public function auth($password)
@@ -227,16 +186,7 @@ class Client
 
     public function call($cmd, $args=[], $direct=false)
     {
-        $command = new Command($cmd, $args);
-
-        if (!$direct) {
-            $this->queue->enqueue($command);
-            $this->process();
-        } else {
-            $this->execCommand($command);
-        }
-
-        return $command->deferred->getFuture()->await();
+        return $this->execute(new Command($cmd, $args), $direct);
     }
 
     public function __call($method, $args)
@@ -244,142 +194,12 @@ class Client
         return $this->call($method, $args);
     }
 
-    private function process()
-    {
-        if ($this->processing || $this->queue->isEmpty() || $this->queuePaused) {
-            return;
-        }
-
-        $this->processing = true;
-
-        EventLoop::queue(function() {
-            while (!$this->queue->isEmpty()) {
-                if ($this->queuePaused) {
-                    break;
-                }
-                /** @var Command $command */
-                $cmd = $this->queue->dequeue();
-                $this->execCommand($cmd);
-            }
-            $this->processing = false;
-        });
-    }
-
-    private function execCommand(Command $cmd)
-    {
-        // echo "Send: ".$cmd->__toString();
-
-        $this->socket->write($cmd->__toString());
-        $buffer = $this->socket->read();
-
-        if ($buffer === null) {
-            echo "Redis connection is closed\n";
-            echo 'Readable: '.($this->socket->isReadable() ? 'true' : 'false')."\n";
-            echo 'Writeable: '.($this->socket->isWritable() ? 'true' : 'false')."\n";
-            echo 'IsClosed: '.($this->socket->isClosed() ? 'true' : 'false')."\n";
-
-            $this->queuePaused = true;
-            $this->queue->enqueue($cmd);
-
-            $this->socket->close();
-            $this->status = self::STATUS_CLOSED;
-
-            $this->connect();
-
-            return;
-        }
-
-        [$type, $data] = $this->decode($buffer);
-
-        //!号代表协议解析错误
-        if ($type !== '-' && $type !== '!') {
-            if ($type === '+' && $data === 'OK') {
-                $cmd->deferred->complete(true);
-            } else {
-                $cmd->deferred->complete($data);
-            }
-        } else {
-            $cmd->deferred->error(new Exception($data));
-        }
-
-        // echo "Get: $type ".(is_array($data) ? print_r($data, true) : $data)."\n";
-    }
-
-    private function decode($buffer)
-    {
-        $type = $buffer[0];
-        switch ($type) {
-            case ':':
-                return [$type, (int) substr($buffer, 1)];
-            case '+':
-                return [$type, \substr($buffer, 1, strlen($buffer) - 3)];
-            case '-':
-                return [$type, \substr($buffer, 1, strlen($buffer) - 3)];
-            case '$':
-                if (0 === strpos($buffer, '$-1')) {
-                    return [$type, null];
-                }
-                $pos = \strpos($buffer, "\r\n");
-                return [$type, \substr($buffer, $pos + 2, (int)substr($buffer, 1, $pos))];
-            case '*':
-                if (0 === strpos($buffer, '*-1')) {
-                    return [$type, null];
-                }
-                $pos = \strpos($buffer, "\r\n");
-                $value = [];
-                $count = (int)substr($buffer, 1, $pos - 1);
-                while ($count --) {
-                    $next_pos = strpos($buffer, "\r\n", $pos + 2);
-                    if (!$next_pos) {
-                        return 0;
-                    }
-                    $sub_type = $buffer[$pos + 2];
-                    switch ($sub_type) {
-                        case ':':
-                            $value[] = (int) substr($buffer, $pos + 3, $next_pos - $pos - 3);
-                            $pos = $next_pos;
-                            break;
-                        case '+':
-                            $value[] = substr($buffer, $pos + 3, $next_pos - $pos - 3);
-                            $pos = $next_pos;
-                            break;
-                        case '-':
-                            $value[] = substr($buffer, $pos + 3, $next_pos - $pos - 3);
-                            $pos = $next_pos;
-                            break;
-                        case '$':
-                            if($pos + 2 === strpos($buffer, '$-1', $pos)) {
-                                $pos = $next_pos;
-                                $value[] = null;
-                                break;
-                            }
-                            $length = (int)substr($buffer, $pos + 3, $next_pos - $pos -3);
-                            $value[] = substr($buffer, $next_pos + 2, $length);
-                            $pos = $next_pos + $length + 2;
-                            break;
-                        default:
-                            return ['!', "protocol error, got '$sub_type' as reply type byte. buffer:".bin2hex($buffer)." pos:$pos"];
-                    }
-                }
-                return [$type, $value];
-            default:
-                return ['!', "protocol error, got '$type' as reply type byte."];
-
-        }
-    }
-
     /**
      * close
      */
-    public function close()
+    protected function cleanResources()
     {
-        if ($this->socket->isClosed()) {
-            return;
-        }
-
         $this->subscribes = [];
-        $this->socket->close();
-        $this->status = self::STATUS_CLOSED;
     }
 
     public function __destruct()
--- src/Command.php
+++ src/Command.php
@@ -3,9 +3,11 @@
 namespace Wind\Redis;
 
 use Amp\DeferredFuture;
+use Amp\Future;
 use Closure;
+use Wind\Socket\SimpleTextCommand;
 
-class Command {
+class Command implements SimpleTextCommand {
 
     public DeferredFuture $deferred;
 
@@ -17,6 +19,119 @@ class Command {
     public function __construct(public string $cmd, public array $args, private ?Closure $callback=null)
     {
         $this->deferred = new DeferredFuture();
+    }
+
+    public function getFuture(): Future
+    {
+        return $this->deferred->getFuture();
+    }
+
+    public function encode(): string
+    {
+        if (count($this->args) === 0) {
+            return strtoupper($this->cmd)."\r\n";
+        }
+
+        $data = [strtoupper($this->cmd), ...$this->args];
+
+        $cmd = '';
+        $count = \count($data);
+        foreach ($data as $item) {
+            if (\is_array($item)) {
+                $count += \count($item) - 1;
+                foreach ($item as $str) {
+                    $cmd .= '$' . \strlen($str) . "\r\n$str\r\n";
+                }
+            } else {
+                $cmd .= '$' . \strlen($item) . "\r\n$item\r\n";
+            }
+        }
+
+        return "*$count\r\n$cmd";
+    }
+
+    private function decode($buffer)
+    {
+        $type = $buffer[0];
+        switch ($type) {
+            case ':':
+                return [$type, (int) substr($buffer, 1)];
+            case '+':
+                return [$type, \substr($buffer, 1, strlen($buffer) - 3)];
+            case '-':
+                return [$type, \substr($buffer, 1, strlen($buffer) - 3)];
+            case '$':
+                if (0 === strpos($buffer, '$-1')) {
+                    return [$type, null];
+                }
+                $pos = \strpos($buffer, "\r\n");
+                return [$type, \substr($buffer, $pos + 2, (int)substr($buffer, 1, $pos))];
+            case '*':
+                if (0 === strpos($buffer, '*-1')) {
+                    return [$type, null];
+                }
+                $pos = \strpos($buffer, "\r\n");
+                $value = [];
+                $count = (int)substr($buffer, 1, $pos - 1);
+                while ($count --) {
+                    $next_pos = strpos($buffer, "\r\n", $pos + 2);
+                    if (!$next_pos) {
+                        return 0;
+                    }
+                    $sub_type = $buffer[$pos + 2];
+                    switch ($sub_type) {
+                        case ':':
+                            $value[] = (int) substr($buffer, $pos + 3, $next_pos - $pos - 3);
+                            $pos = $next_pos;
+                            break;
+                        case '+':
+                            $value[] = substr($buffer, $pos + 3, $next_pos - $pos - 3);
+                            $pos = $next_pos;
+                            break;
+                        case '-':
+                            $value[] = substr($buffer, $pos + 3, $next_pos - $pos - 3);
+                            $pos = $next_pos;
+                            break;
+                        case '$':
+                            if($pos + 2 === strpos($buffer, '$-1', $pos)) {
+                                $pos = $next_pos;
+                                $value[] = null;
+                                break;
+                            }
+                            $length = (int)substr($buffer, $pos + 3, $next_pos - $pos -3);
+                            $value[] = substr($buffer, $next_pos + 2, $length);
+                            $pos = $next_pos + $length + 2;
+                            break;
+                        default:
+                            return ['!', "protocol error, got '$sub_type' as reply type byte. buffer:".bin2hex($buffer)." pos:$pos"];
+                    }
+                }
+                return [$type, $value];
+            default:
+                return ['!', "protocol error, got '$type' as reply type byte."];
+
+        }
+    }
+
+    public function resolve(string|\Throwable $buffer)
+    {
+        if ($buffer instanceof \Throwable) {
+            $this->deferred->error($buffer);
+            return;
+        }
+
+        [$type, $data] = $this->decode($buffer);
+
+        //!号代表协议解析错误
+        if ($type !== '-' && $type !== '!') {
+            if ($type === '+' && $data === 'OK') {
+                $this->deferred->complete(true);
+            } else {
+                $this->deferred->complete($data);
+            }
+        } else {
+            $this->deferred->error(new Exception($data));
+        }
     }
 
     /**
@@ -34,30 +149,6 @@ class Command {
             $closure = $this->callback;
             $closure();
         }
-    }
-
-    public function __toString(): string
-    {
-        if (count($this->args) === 0) {
-            return $this->cmd."\r\n";
-        }
-
-        $data = [$this->cmd, ...$this->args];
-
-        $cmd = '';
-        $count = \count($data);
-        foreach ($data as $item) {
-            if (\is_array($item)) {
-                $count += \count($item) - 1;
-                foreach ($item as $str) {
-                    $cmd .= '$' . \strlen($str) . "\r\n$str\r\n";
-                }
-            } else {
-                $cmd .= '$' . \strlen($item) . "\r\n$item\r\n";
-            }
-        }
-
-        return "*$count\r\n$cmd";
     }
 
 }
