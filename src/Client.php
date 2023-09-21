3b55580071159f783ebd5439cb82ab81a8930134
Pader <ypnow@163.com>
2023-09-22 00:07:24 +0800

    Use new redis client base on amp/socket as main client.

--- src/Client.php
+++ src/Client.php
@@ -2,20 +2,35 @@
 
 namespace Wind\Redis;
 
-use Amp\Socket\ConnectContext;
-use Amp\Socket\Socket;
-use Amp\Socket\SocketConnector;
-use Wind\Socket\SimpleTextClient;
+use Amp\DeferredFuture;
+use Revolt\EventLoop;
+use SplQueue;
+use Workerman\Connection\AsyncTcpConnection;
+use Workerman\Connection\TcpConnection;
+use Workerman\Timer;
 
 /**
- * Wind Framework Redis Client
+ * Wind Redis Client
  */
-class Client extends SimpleTextClient
+class Client
 {
 
     use Commands;
 
+    private AsyncTcpConnection $connection;
+
+    private int $status = 0;
+
+    private const STATUS_CLOSED = 0;
+    private const STATUS_CONNECTING = 1;
+    private const STATUS_CONNECTED = 2;
+
+    private ?DeferredFuture $connectDeferred;
+
     private array $config = [];
+
+    private SplQueue $queue;
+    private bool $queuePaused = false;
 
     /**
      * @var int
@@ -23,14 +38,21 @@ class Client extends SimpleTextClient
     private $db = 0;
 
     /**
-     * @var string|array|null
+     * @var string|array
      */
     private $password = null;
+
+    /**
+     * @var string
+     */
+    private $lastError = '';
 
     /**
      * @var array
      */
     private $subscribes = [];
+
+    private ?Command $pending = null;
 
     /**
      * Create redis connection instance
@@ -39,57 +61,213 @@ class Client extends SimpleTextClient
      */
     public function __construct($connection='default')
     {
-        parent::__construct();
+        $this->config = is_array($connection) ? $connection : config('redis.'.$connection);
 
-        if (is_array($connection)) {
-            $this->config = $connection;
-        } else {
-            $this->config = config('redis.'.$connection);
-            if ($this->config === null) {
-                throw new Exception("Unable to find config for redis connection '$connection'.");
-            }
+        if ($this->config === null) {
+            throw new Exception("Unable to find config for redis connection '$connection'.");
         }
 
-        $this->autoReconnect = $this->config['auto_reconnect'] ?? true;
-
-        if (isset($this->config['reconnect_delay'])) {
-            $this->reconnectDelay = $this->config['reconnect_delay'];
+        if (!\class_exists('Protocols\Redis')) {
+            \class_alias('Wind\Redis\Protocol', 'Protocols\Redis');
         }
+
+        $address = 'redis://'.$this->config['host'].':'.($this->config['port'] ?? 6379);
+        $this->connection = new AsyncTcpConnection($address, $this->config['context'] ?? []);
+
+        $this->queue = new SplQueue;
+        $this->queue->setIteratorMode(SplQueue::IT_MODE_DELETE);
 
         $this->connect();
     }
 
-    protected function createSocket(SocketConnector $connector): Socket
+    /**
+     * connect
+     *
+     * @return void
+     */
+    public function connect()
     {
-        $address = 'tcp://'.$this->config['host'].':'.($this->config['port'] ?? 6379);
-        $timeout = $this->config['connect_timeout'] ?? 5;
-
-        $connectContext = (new ConnectContext)
-            ->withConnectTimeout($timeout);
-
-        return $connector->connect($address, $connectContext);
-    }
-
-    protected function authenticate()
-    {
-        if ($this->password || !empty($this->config['auth'])) {
-            $this->auth($this->password ?: $this->config['auth']);
+        if ($this->isConnected()) {
+            return;
         }
 
-        $this->select($this->db);
+        $timeout = $this->config['connect_timeout'] ?? 5;
+        $reconnect = $this->config['auto_reconnect'] ?? true;
+
+        //连接超时设置
+        $connectTimer = Timer::add($timeout, function() {
+            $this->connection->destroy();
+            $this->connectDeferred?->error(new Exception('Connect to redis timeout.'));
+            $this->connectDeferred = null;
+        }, [], false);
+
+        $this->connection->onConnect = function () use (&$connectTimer, $reconnect) {
+            if ($connectTimer) {
+                Timer::del($connectTimer);
+                $connectTimer = null;
+            }
+
+            try {
+                if ($this->password || !empty($this->config['auth'])) {
+                    // echo "Auth...\n";
+                    $this->auth($this->password ?: $this->config['auth']);
+                }
+
+                if ($this->db || isset($this->config['db'])) {
+                    // echo "Select db...\n";
+                    $this->select($this->db ?: $this->config['db']);
+                }
+
+                $this->lastError = '';
+                $this->status = self::STATUS_CONNECTED;
+
+                $this->connectDeferred->complete();
+                $this->connectDeferred = null;
+
+                $this->queuePaused = false;
+
+                $this->process();
+
+            } catch (Exception $e) {
+                $this->connectDeferred?->error($e);
+                $this->connectDeferred = null;
+            }
+        };
+
+        $this->connection->onError = function ($connection, $code, $message) use (&$connectTimer, $reconnect) {
+            if ($connectTimer) {
+                Timer::del($connectTimer);
+                $connectTimer = null;
+            }
+
+            $msg = "[$code] $message";
+            $this->lastError = $msg;
+        };
+
+        $this->connection->onClose = function () use (&$connectTimer, $reconnect) {
+            if ($connectTimer) {
+                Timer::del($connectTimer);
+                $connectTimer = null;
+            }
+
+            $this->queuePaused = true;
+
+            if ($reconnect) {
+                //将当前队列的内容先重新放进队列
+                if ($this->pending) {
+                    $this->queue->enqueue($this->pending);
+                    $this->pending = null;
+                }
+
+                //自动重连时等待并尝试重连
+                $reconnectDelay = $this->config['reconnect_delay'] ?? 2;
+                echo "Reconnect after {$reconnectDelay} seconds cause: {$this->lastError}.\n";
+                $this->status = self::STATUS_CONNECTING;
+                Timer::add($reconnectDelay, fn() => EventLoop::queue($this->connect(...)), [], false);
+            } else {
+                $error = new Exception($this->lastError ?: 'Disconnected.');
+                $this->status = self::STATUS_CLOSED;
+
+                //不自动重连时，所有队列都报连接关闭错误
+                if ($this->pending) {
+                    $this->pending->deferred->error($error);
+                    $this->pending = null;
+                }
+
+                while (!$this->queue->isEmpty()) {
+                    $pending = $this->queue->dequeue();
+                    $pending->deferred->error($error);
+                }
+
+                $this->connectDeferred?->error($error);
+                $this->connectDeferred = null;
+            }
+        };
+
+        $this->connection->onMessage = function ($connection, $data) {
+            [$type, $result] = $data;
+
+            //!号代表协议解析错误
+            if ($type !== '-' && $type !== '!') {
+                $this->lastError = '';
+
+                if ($type === '+' && $result === 'OK') {
+                    $result = true;
+                } elseif (is_array($result) && $this->subscribes && ($result[0] == 'message' || $result[0] == 'pmessage')) {
+                    //来自订阅的消息
+                    $channel = $result[1];
+                    if (isset($this->subscribes[$channel])) {
+                        $this->subscribes[$channel](array_slice($result, 1));
+                    } else {
+                        echo "Unknown subscribe of channel {$channel}.\n";
+                    }
+                    return;
+                }
+            } else {
+                $this->lastError = $result;
+                $result = false;
+            }
+
+            if ($this->pending) {
+                $pending = $this->pending;
+                $this->pending = null;
+
+                if ($result !== false) {
+                    $pending->deferred->complete($result);
+                } else {
+                    $pending->deferred->error(new Exception($this->lastError));
+                }
+
+                $pending->callback();
+            }
+
+            if ($type === '!') {
+                $this->close();
+                $this->connect();
+            } else {
+                $this->process();
+            }
+        };
+
+        $this->connectDeferred ??= new DeferredFuture();
+        $this->status = self::STATUS_CONNECTING;
+
+        EventLoop::defer($this->connection->connect(...));
+
+        $this->connectDeferred->getFuture()->await();
     }
 
-    public function auth($password)
+    public function isConnected()
     {
-        $result = $this->call('AUTH', [$password], $this->status == self::STATUS_CONNECTING);
-        $this->password = $password;
-        return $result;
+        return $this->status == self::STATUS_CONNECTED;
+    }
+
+    /**
+     * process
+     */
+    protected function process()
+    {
+        if ($this->pending !== null || $this->queue->isEmpty() || $this->queuePaused) {
+            return;
+        }
+
+        /** @var Command $command */
+        $command = $this->queue->dequeue();
+        $this->connection->send($command->buffer());
+        $this->pending = $command;
     }
 
     public function select($db)
     {
         $result = $this->call('SELECT', [$db], $this->status == self::STATUS_CONNECTING);
         $this->db = $db;
+        return $result;
+    }
+
+    public function auth($password)
+    {
+        $result = $this->call('AUTH', [$password], $this->status == self::STATUS_CONNECTING);
+        $this->password = $password;
         return $result;
     }
 
@@ -117,7 +295,6 @@ class Client extends SimpleTextClient
         return $result;
     }
 
-
     /**
      * psubscribe
      *
@@ -142,23 +319,25 @@ class Client extends SimpleTextClient
         return $result;
     }
 
-    protected function multi()
+    public function multi()
     {
-        $this->call('MULTI');
-        $this->queuePaused = true;
+        return $this->call('MULTI', [], false, function() {
+            $this->queuePaused = true;
+        });
     }
 
-    protected function exec()
+    public function exec()
     {
-        $arr = $this->call('EXEC', direct: true);
-        $this->queuePaused = false;
-        return $arr;
+        return $this->call('EXEC', [], true, function() {
+            $this->queuePaused = false;
+        });
     }
 
-    protected function discard()
+    public function discard()
     {
-        $this->call('DISCARD', direct: true);
-        $this->queuePaused = false;
+        return $this->call('DISCARD', [], true, function() {
+            $this->queuePaused = false;
+        });
     }
 
     /**
@@ -175,7 +354,6 @@ class Client extends SimpleTextClient
             $inTransactionCallback($transaction);
             return $this->exec();
         } catch (\Throwable $e) {
-            echo $e;
             $this->discard();
             throw $e;
         } finally {
@@ -183,9 +361,31 @@ class Client extends SimpleTextClient
         }
     }
 
-    public function call($cmd, $args=[], $direct=false)
+    /**
+     * 发送指令
+     *
+     * @param string $cmd
+     * @param array $args
+     * @param boolean $direct Directly send or into the queue
+     * @param \Closure $callback Atomic callback, see Command
+     */
+    public function call($cmd, $args, $direct=false, $callback=null)
     {
-        return $this->execute(new Command($cmd, $args), $direct);
+        if ($this->status == self::STATUS_CLOSED) {
+            throw new Exception($this->lastError ?: 'Lost connection.');
+        }
+
+        $command = new Command($cmd, $args, $callback);
+
+        if (!$direct) {
+            $this->queue->enqueue($command);
+            $this->process();
+        } else {
+            $this->connection->send($command->buffer());
+            $this->pending = $command;
+        }
+
+        return $command->deferred->getFuture()->await();
     }
 
     public function __call($method, $args)
@@ -194,16 +394,72 @@ class Client extends SimpleTextClient
     }
 
     /**
+     * error
+     *
+     * @return string
+     */
+    public function getLastError()
+    {
+        return $this->lastError;
+    }
+
+    /**
      * close
      */
-    protected function cleanResources()
+    public function close()
     {
+        if ($this->connection->getStatus() == TcpConnection::STATUS_CLOSED) {
+            return;
+        }
+
         $this->subscribes = [];
+        $this->connection->onConnect = $this->connection->onError = $this->connection->onClose = $this->connection->onMessage = null;
+        $this->connection->close();
     }
 
     public function __destruct()
     {
         $this->close();
+    }
+
+    /**
+     * scan
+     *
+     * @throws Exception
+     */
+    public function scan()
+    {
+        throw new Exception('Not implemented');
+    }
+
+    /**
+     * hScan
+     *
+     * @throws Exception
+     */
+    public function hScan()
+    {
+        throw new Exception('Not implemented');
+    }
+
+    /**
+     * hScan
+     *
+     * @throws Exception
+     */
+    public function sScan()
+    {
+        throw new Exception('Not implemented');
+    }
+
+    /**
+     * hScan
+     *
+     * @throws Exception
+     */
+    public function zScan()
+    {
+        throw new Exception('Not implemented');
     }
 
 }
--- src/Redis.php
+++ src/Redis.php
@@ -2,35 +2,20 @@
 
 namespace Wind\Redis;
 
-use Amp\DeferredFuture;
-use Revolt\EventLoop;
-use SplQueue;
-use Workerman\Connection\AsyncTcpConnection;
-use Workerman\Connection\TcpConnection;
-use Workerman\Timer;
+use Amp\Socket\ConnectContext;
+use Amp\Socket\Socket;
+use Amp\Socket\SocketConnector;
+use Wind\Socket\SimpleTextClient;
 
 /**
- * Wind Redis Client
+ * Wind Framework Redis Client
  */
-class Redis
+class Redis extends SimpleTextClient
 {
 
     use Commands;
 
-    private AsyncTcpConnection $connection;
-
-    private int $status = 0;
-
-    private const STATUS_CLOSED = 0;
-    private const STATUS_CONNECTING = 1;
-    private const STATUS_CONNECTED = 2;
-
-    private ?DeferredFuture $connectDeferred;
-
     private array $config = [];
-
-    private SplQueue $queue;
-    private bool $queuePaused = false;
 
     /**
      * @var int
@@ -38,21 +23,14 @@ class Redis
     private $db = 0;
 
     /**
-     * @var string|array
+     * @var string|array|null
      */
     private $password = null;
-
-    /**
-     * @var string
-     */
-    private $lastError = '';
 
     /**
      * @var array
      */
     private $subscribes = [];
-
-    private ?Command $pending = null;
 
     /**
      * Create redis connection instance
@@ -61,213 +39,57 @@ class Redis
      */
     public function __construct($connection='default')
     {
-        $this->config = is_array($connection) ? $connection : config('redis.'.$connection);
+        parent::__construct();
 
-        if ($this->config === null) {
-            throw new Exception("Unable to find config for redis connection '$connection'.");
+        if (is_array($connection)) {
+            $this->config = $connection;
+        } else {
+            $this->config = config('redis.'.$connection);
+            if ($this->config === null) {
+                throw new Exception("Unable to find config for redis connection '$connection'.");
+            }
         }
 
-        if (!\class_exists('Protocols\Redis')) {
-            \class_alias('Wind\Redis\Protocol', 'Protocols\Redis');
+        $this->autoReconnect = $this->config['auto_reconnect'] ?? true;
+
+        if (isset($this->config['reconnect_delay'])) {
+            $this->reconnectDelay = $this->config['reconnect_delay'];
         }
-
-        $address = 'redis://'.$this->config['host'].':'.($this->config['port'] ?? 6379);
-        $this->connection = new AsyncTcpConnection($address, $this->config['context'] ?? []);
-
-        $this->queue = new SplQueue;
-        $this->queue->setIteratorMode(SplQueue::IT_MODE_DELETE);
 
         $this->connect();
     }
 
-    /**
-     * connect
-     *
-     * @return void
-     */
-    public function connect()
+    protected function createSocket(SocketConnector $connector): Socket
     {
-        if ($this->isConnected()) {
-            return;
-        }
-
+        $address = 'tcp://'.$this->config['host'].':'.($this->config['port'] ?? 6379);
         $timeout = $this->config['connect_timeout'] ?? 5;
-        $reconnect = $this->config['auto_reconnect'] ?? true;
 
-        //连接超时设置
-        $connectTimer = Timer::add($timeout, function() {
-            $this->connection->destroy();
-            $this->connectDeferred?->error(new Exception('Connect to redis timeout.'));
-            $this->connectDeferred = null;
-        }, [], false);
+        $connectContext = (new ConnectContext)
+            ->withConnectTimeout($timeout);
 
-        $this->connection->onConnect = function () use (&$connectTimer, $reconnect) {
-            if ($connectTimer) {
-                Timer::del($connectTimer);
-                $connectTimer = null;
-            }
-
-            try {
-                if ($this->password || !empty($this->config['auth'])) {
-                    // echo "Auth...\n";
-                    $this->auth($this->password ?: $this->config['auth']);
-                }
-
-                if ($this->db || isset($this->config['db'])) {
-                    // echo "Select db...\n";
-                    $this->select($this->db ?: $this->config['db']);
-                }
-
-                $this->lastError = '';
-                $this->status = self::STATUS_CONNECTED;
-
-                $this->connectDeferred->complete();
-                $this->connectDeferred = null;
-
-                $this->queuePaused = false;
-
-                $this->process();
-
-            } catch (Exception $e) {
-                $this->connectDeferred?->error($e);
-                $this->connectDeferred = null;
-            }
-        };
-
-        $this->connection->onError = function ($connection, $code, $message) use (&$connectTimer, $reconnect) {
-            if ($connectTimer) {
-                Timer::del($connectTimer);
-                $connectTimer = null;
-            }
-
-            $msg = "[$code] $message";
-            $this->lastError = $msg;
-        };
-
-        $this->connection->onClose = function () use (&$connectTimer, $reconnect) {
-            if ($connectTimer) {
-                Timer::del($connectTimer);
-                $connectTimer = null;
-            }
-
-            $this->queuePaused = true;
-
-            if ($reconnect) {
-                //将当前队列的内容先重新放进队列
-                if ($this->pending) {
-                    $this->queue->enqueue($this->pending);
-                    $this->pending = null;
-                }
-
-                //自动重连时等待并尝试重连
-                $reconnectDelay = $this->config['reconnect_delay'] ?? 2;
-                echo "Reconnect after {$reconnectDelay} seconds cause: {$this->lastError}.\n";
-                $this->status = self::STATUS_CONNECTING;
-                Timer::add($reconnectDelay, fn() => EventLoop::queue($this->connect(...)), [], false);
-            } else {
-                $error = new Exception($this->lastError ?: 'Disconnected.');
-                $this->status = self::STATUS_CLOSED;
-
-                //不自动重连时，所有队列都报连接关闭错误
-                if ($this->pending) {
-                    $this->pending->deferred->error($error);
-                    $this->pending = null;
-                }
-
-                while (!$this->queue->isEmpty()) {
-                    $pending = $this->queue->dequeue();
-                    $pending->deferred->error($error);
-                }
-
-                $this->connectDeferred?->error($error);
-                $this->connectDeferred = null;
-            }
-        };
-
-        $this->connection->onMessage = function ($connection, $data) {
-            [$type, $result] = $data;
-
-            //!号代表协议解析错误
-            if ($type !== '-' && $type !== '!') {
-                $this->lastError = '';
-
-                if ($type === '+' && $result === 'OK') {
-                    $result = true;
-                } elseif (is_array($result) && $this->subscribes && ($result[0] == 'message' || $result[0] == 'pmessage')) {
-                    //来自订阅的消息
-                    $channel = $result[1];
-                    if (isset($this->subscribes[$channel])) {
-                        $this->subscribes[$channel](array_slice($result, 1));
-                    } else {
-                        echo "Unknown subscribe of channel {$channel}.\n";
-                    }
-                    return;
-                }
-            } else {
-                $this->lastError = $result;
-                $result = false;
-            }
-
-            if ($this->pending) {
-                $pending = $this->pending;
-                $this->pending = null;
-
-                if ($result !== false) {
-                    $pending->deferred->complete($result);
-                } else {
-                    $pending->deferred->error(new Exception($this->lastError));
-                }
-
-                $pending->callback();
-            }
-
-            if ($type === '!') {
-                $this->close();
-                $this->connect();
-            } else {
-                $this->process();
-            }
-        };
-
-        $this->connectDeferred ??= new DeferredFuture();
-        $this->status = self::STATUS_CONNECTING;
-
-        EventLoop::defer($this->connection->connect(...));
-
-        $this->connectDeferred->getFuture()->await();
+        return $connector->connect($address, $connectContext);
     }
 
-    public function isConnected()
+    protected function authenticate()
     {
-        return $this->status == self::STATUS_CONNECTED;
-    }
-
-    /**
-     * process
-     */
-    protected function process()
-    {
-        if ($this->pending !== null || $this->queue->isEmpty() || $this->queuePaused) {
-            return;
+        if ($this->password || !empty($this->config['auth'])) {
+            $this->auth($this->password ?: $this->config['auth']);
         }
 
-        /** @var Command $command */
-        $command = $this->queue->dequeue();
-        $this->connection->send($command->buffer());
-        $this->pending = $command;
-    }
-
-    public function select($db)
-    {
-        $result = $this->call('SELECT', [$db], $this->status == self::STATUS_CONNECTING);
-        $this->db = $db;
-        return $result;
+        $this->select($this->db);
     }
 
     public function auth($password)
     {
         $result = $this->call('AUTH', [$password], $this->status == self::STATUS_CONNECTING);
         $this->password = $password;
+        return $result;
+    }
+
+    public function select($db)
+    {
+        $result = $this->call('SELECT', [$db], $this->status == self::STATUS_CONNECTING);
+        $this->db = $db;
         return $result;
     }
 
@@ -295,6 +117,7 @@ class Redis
         return $result;
     }
 
+
     /**
      * psubscribe
      *
@@ -319,25 +142,23 @@ class Redis
         return $result;
     }
 
-    public function multi()
+    protected function multi()
     {
-        return $this->call('MULTI', [], false, function() {
-            $this->queuePaused = true;
-        });
+        $this->call('MULTI');
+        $this->queuePaused = true;
     }
 
-    public function exec()
+    protected function exec()
     {
-        return $this->call('EXEC', [], true, function() {
-            $this->queuePaused = false;
-        });
+        $arr = $this->call('EXEC', direct: true);
+        $this->queuePaused = false;
+        return $arr;
     }
 
-    public function discard()
+    protected function discard()
     {
-        return $this->call('DISCARD', [], true, function() {
-            $this->queuePaused = false;
-        });
+        $this->call('DISCARD', direct: true);
+        $this->queuePaused = false;
     }
 
     /**
@@ -354,6 +175,7 @@ class Redis
             $inTransactionCallback($transaction);
             return $this->exec();
         } catch (\Throwable $e) {
+            echo $e;
             $this->discard();
             throw $e;
         } finally {
@@ -361,31 +183,9 @@ class Redis
         }
     }
 
-    /**
-     * 发送指令
-     *
-     * @param string $cmd
-     * @param array $args
-     * @param boolean $direct Directly send or into the queue
-     * @param \Closure $callback Atomic callback, see Command
-     */
-    public function call($cmd, $args, $direct=false, $callback=null)
+    public function call($cmd, $args=[], $direct=false)
     {
-        if ($this->status == self::STATUS_CLOSED) {
-            throw new Exception($this->lastError ?: 'Lost connection.');
-        }
-
-        $command = new Command($cmd, $args, $callback);
-
-        if (!$direct) {
-            $this->queue->enqueue($command);
-            $this->process();
-        } else {
-            $this->connection->send($command->buffer());
-            $this->pending = $command;
-        }
-
-        return $command->deferred->getFuture()->await();
+        return $this->execute(new Command($cmd, $args), $direct);
     }
 
     public function __call($method, $args)
@@ -394,72 +194,16 @@ class Redis
     }
 
     /**
-     * error
-     *
-     * @return string
-     */
-    public function getLastError()
-    {
-        return $this->lastError;
-    }
-
-    /**
      * close
      */
-    public function close()
+    protected function cleanResources()
     {
-        if ($this->connection->getStatus() == TcpConnection::STATUS_CLOSED) {
-            return;
-        }
-
         $this->subscribes = [];
-        $this->connection->onConnect = $this->connection->onError = $this->connection->onClose = $this->connection->onMessage = null;
-        $this->connection->close();
     }
 
     public function __destruct()
     {
         $this->close();
-    }
-
-    /**
-     * scan
-     *
-     * @throws Exception
-     */
-    public function scan()
-    {
-        throw new Exception('Not implemented');
-    }
-
-    /**
-     * hScan
-     *
-     * @throws Exception
-     */
-    public function hScan()
-    {
-        throw new Exception('Not implemented');
-    }
-
-    /**
-     * hScan
-     *
-     * @throws Exception
-     */
-    public function sScan()
-    {
-        throw new Exception('Not implemented');
-    }
-
-    /**
-     * hScan
-     *
-     * @throws Exception
-     */
-    public function zScan()
-    {
-        throw new Exception('Not implemented');
     }
 
 }
